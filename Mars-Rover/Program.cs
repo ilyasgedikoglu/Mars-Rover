aeb124fb03971ba017ff0a1d540bfaa1110a2d20
ilyas gedikoğlu <igedikoglugs@hotmail.com>
2022-02-27 18:45:56 +0300

    Project codes were written. Rover class created. The main function has been written. Comments have been written.

--- Mars-Rover/Program.cs
+++ Mars-Rover/Program.cs
@@ -1,12 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mars_Rover
 {
     internal class Program
     {
+        /*
+         * İLYAS GEDİKOĞLU - MARS ROVER PROJECT
+         */
+
+        /*
+         * After entering the input values, press the enter key to see the output value.
+         */
+
+        //It is the list variable that will hold the input values.
+        static List<string> inputList = new List<string>();
+
+        //It is the variable that will hold the upper right coordinate value.
+        static string[] upperRightCoordinates = new string[3];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //We get the upper right coordinate value from the user.
+            var upperRightCoordinate = Console.ReadLine();
+            if (!string.IsNullOrEmpty(upperRightCoordinate))
+            {
+                //If the value is not empty, we divide the value according to the spaces and assign it to our variable.
+                upperRightCoordinates = upperRightCoordinate.Split(' ');
+
+                if (upperRightCoordinates.Length == 2)
+                {
+                    //I created an infinite loop to enter an input value until the enter key is pressed.
+                    while (true)
+                    {
+                        var input = Console.ReadLine();
+
+                        //If the entered value is empty, the loop is exited.
+                        if (string.IsNullOrEmpty(input))
+                            break;
+                        else
+                            //If the entered value is not empty, it is added to the input list.
+                            inputList.Add(input);
+                    }
+
+                    //This function is called to start the navigation when the input entries are finished.
+                    StartRover();
+                }
+                else
+                    Console.WriteLine("Plateau coordinates were entered incorrectly.");
+            }
+        }
+
+        static void StartRover ()
+        {
+            //Each rover has two input values. So we increment the i of your loop by 2.
+            for (int i = 0; i < inputList.Count; i+=2)
+            {
+                //The input value where the coordinate and direction is specified is checked.
+                if (string.IsNullOrEmpty(inputList[i]))
+                    Console.WriteLine("The input value was not found.");
+                else
+                {
+                    //The input value specifying the coordinate and direction is divided by looking at the spaces.
+                    var input1 = inputList[i].Split(' ');
+
+                    //Checking the accuracy of the input value.
+                    if (input1.Length < 3 || input1.Length > 3)
+                        Console.WriteLine("Rover's location information is missing or entered incorrectly.");
+                    else
+                    {
+                        //Creating the Rover object. The first parameter is the x coordinate, the second parameter is the y coordinate, and the third parameter is the direction.
+                        Rover rover = new Rover(Convert.ToInt32(input1[0]), Convert.ToInt32(input1[1]), Convert.ToChar(input1[2]));
+
+                        //The input value that specifies the movements to be made by the rover is checked.
+                        if (string.IsNullOrEmpty(inputList[i + 1]))
+                        {
+                            Console.WriteLine("The list of moves that the Rover will explore the plateau has not been sent.");
+                        }
+                        else
+                        {
+                            //The letters in the input value indicating the movements that the rover will do are looked at.
+                            for (int y = 0; y < inputList[i + 1].Length; y++)
+                            {
+                                //If the letter read is L, the "Left" function is called to make the rover turn left.
+                                if (inputList[i + 1][y] == 'L')
+                                    rover.Left();
+                                //If the letter read is L, the "Right" function is called to make the rover turn left.
+                                else if (inputList[i + 1][y] == 'R')
+                                    rover.Right();
+                                //If the letter read is M, the "Move" function is called to move the rover 1 grid forward.
+                                else if (inputList[i + 1][y] == 'M')
+                                    rover.Move(Convert.ToInt32(upperRightCoordinates[0]), Convert.ToInt32(upperRightCoordinates[1]));
+                                //If the letter read is not L, R or M, an incorrect input message is returned.
+                                else
+                                    Console.WriteLine("The list of moves for which Rover will explore the plateau has been sent incorrectly.");
+                            }
+
+                            //The rover's output is printed on the screen.
+                            Console.WriteLine(rover.Get_X_Coordinate() + " " + rover.Get_Y_Coordinate() + " " + rover.Get_Direction());
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ Mars-Rover/Rover.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Rover
+{
+    public class Rover
+    {
+        public Rover(int x, int y, char direction)
+        {
+            Direction = direction;
+            X_Coordinate = x;
+            Y_Coordinate = y;
+        }
+
+        //It is the variable that holds the direction of the rover.
+        private char Direction { get; set; }
+
+        //It is the variable that holds the x coordinate value of the rover.
+        private int X_Coordinate { get; set; }
+
+        //It is the variable that holds the y coordinate value of the rover.
+        private int Y_Coordinate { get; set; }
+
+        //It is the function that returns the x coordinate value of the rover.
+        public int Get_X_Coordinate()
+        {
+            return X_Coordinate;
+        }
+
+        //It is the function that returns the y coordinate value of the rover.
+        public int Get_Y_Coordinate()
+        {
+            return Y_Coordinate;
+        }
+
+        //It is the function that returns the rover's direction information.
+        public char Get_Direction()
+        {
+            return Direction;
+        }
+
+        //It is the function that makes the rover turn left.
+        public void Left ()
+        {
+            if (Direction == 'E')
+                Direction = 'N';
+            else if (Direction == 'N')
+                Direction = 'W';
+            else if (Direction == 'W')
+                Direction = 'S';
+            else
+                Direction = 'E';           
+        }
+
+        //It is the function that makes the rover turn right.
+        public void Right()
+        {
+            if (Direction == 'E')
+                Direction = 'S';
+            else if (Direction == 'N')
+                Direction = 'E';
+            else if (Direction == 'W')
+                Direction = 'N';
+            else
+                Direction = 'W';
+        }
+
+        //It is the function that makes the rover go forward 1 grid.
+        public void Move(int plato_X_Coordinate, int plato_Y_Coordinate)
+        {
+            if (Direction == 'E')
+                //When the rover moves, it is checked whether it stays within the plateau.
+                if (plato_X_Coordinate > X_Coordinate)
+                    X_Coordinate++;
+                else
+                    Console.WriteLine("Invalid move! Rover has come off the plateau.");
+            else if (Direction == 'N')
+                //When the rover moves, it is checked whether it stays within the plateau.
+                if (plato_Y_Coordinate > Y_Coordinate)
+                    Y_Coordinate++;
+                else
+                    Console.WriteLine("Invalid move! Rover has come off the plateau.");
+            else if (Direction == 'W')
+                //When the rover moves, it is checked whether it stays within the plateau.
+                if (X_Coordinate > 0)
+                    X_Coordinate--;
+                else
+                    Console.WriteLine("Invalid move! Rover has come off the plateau.");
+            else
+            //When the rover moves, it is checked whether it stays within the plateau.
+                if (Y_Coordinate > 0)
+                    Y_Coordinate--;
+                else
+                    Console.WriteLine("Invalid move! Rover has come off the plateau.");
+        }
+    }
+}
